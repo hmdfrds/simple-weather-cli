7047220906d728a330f5e8fb5c7c6f8db91e254d
b2mohfi22j <firdaus.mohamad@swisslog.com>
2025-04-03 19:42:03 +0800

    changes

--- get_weather.py
+++ get_weather.py
@@ -3,3 +3,47 @@ import requests
 API_KEY = "20b187ce9fc97eeaed3ea122dafee5e1"
 
 BASE_URL = f"http://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(city_name):
+    url = f"{BASE_URL}?q={city_name}&appid={API_KEY}&unites=metric"
+
+    try:
+        response = requests.get(url, timeout=10)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            city = data.get("name")
+            temp = data.get("main", {}).get("temp")
+            feels_like = data.get("main",{}).get("feels_like")
+            description = data.get("weather",[{}][0]).get("description")
+            humidity = data.get("main", {}).get("humidity")
+            wind_speed = data.get("wind", {}).get("speed")
+
+            print(f"Weather in {city}:")
+            print("-", *25)
+            print(f"Temperature:    {temp}C")
+            print(f"Feels Like: {feels_like}")
+            print(f"Description:    {description}")
+            print(f"Humidity:   {humidity}%")
+            print(f"Wind Speed: {wind_speed} m/s)")
+
+        elif response.status_code== 404:
+            print("Error: City not found. Please check the spelling.")
+        elif response.status_code == 401:
+            print("Error: Authentication failed. Please check your API key.")
+        else:
+            print("Error: Could not retrieve weather data. Check your connection or the API service.")
+    except requests.exceptions.RequestException:
+        print("Error: Could not retrieve weather data. Check your connection or the API service.")
+
+
+if __name__ == "__main__":
+    print("--- Simple Weather Reporter ---")
+    city = input("Enter city name: ")
+    if not city.strip():
+        print("Error: No city name provided.")
+    else:
+        get_weather(city)
+
+        
